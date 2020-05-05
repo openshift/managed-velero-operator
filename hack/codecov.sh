6f06bf42a4044c9f65e182e9574f7601f1bfbefa
Christoph Blecker <cblecker@redhat.com>
2020-05-05 11:52:46 -0700

    Fix uploader script

--- hack/codecov.sh
+++ hack/codecov.sh
@@ -13,7 +13,7 @@ CI_SERVER_URL=https://openshift-gce-devel.appspot.com/build/origin-ci-test
 # Configure the git refs and job link based on how the job was triggered via prow
 if [[ "$JOB_TYPE" == "presubmit" ]]; then
 	echo "detected PR code coverage job for #${PULL_NUMBER}"
-	REF_FLAGS="-P ${PULL_NUMBER} -C ${PULL_BASE_SHA}"
+	REF_FLAGS="-P ${PULL_NUMBER} -C ${PULL_PULL_SHA}"
 	JOB_LINK=${CI_SERVER_URL}/pr-logs/pull/${REPO_OWNER}_${REPO_NAME}/${PULL_NUMBER}/${JOB_NAME}/${BUILD_ID}
 elif [[ "$JOB_TYPE" == "postsubmit" ]]; then
 	echo "detected branch code coverage job for ${PULL_BASE_REF}"

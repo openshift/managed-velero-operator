f05b72aa68ac80f784f47c46d74df610b8509e54
cgong <cgong@redhat.com>
2025-07-02 14:39:54 +1200

    update depend image to 1.2.5-3

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ var (
 
 const (
 	OperatorName      = "managed-velero-operator"
-	VeleroImageTag    = "oadp-velero-rhel8@sha256:bdf6c3b957c8e4b02d83559c03a4cbbb08fd893eed6f3b04f9164cdddf9c3868"                // registry.redhat.io/oadp/oadp-velero-rhel8:1.2.0-37
-	VeleroAwsImageTag = "oadp-velero-plugin-for-aws-rhel8@sha256:2ae11ce320b0383bb2fc98eaf8b70d8ffa3269b0c0b83d80598d65a25aa1190f" // registry.redhat.io/oadp/oadp-velero-plugin-for-aws-rhel8:1.2.0-21
-	VeleroGcpImageTag = "oadp-velero-plugin-for-gcp-rhel8@sha256:42b3c8b0f027a96fef8aaa913ea73537a84667284f198309ae76c4693a564851" // registry.redhat.io/oadp/oadp-velero-plugin-for-gcp-rhel8:1.2.0-22
+	VeleroImageTag    = "oadp-velero-rhel8@sha256:035f48844600bd3beebd6740bf85cf54d98a9232f01c31621d4e995ff366690a"                // registry.redhat.io/oadp/oadp-velero-rhel8:1.2.5-3
+	VeleroAwsImageTag = "oadp-velero-plugin-for-aws-rhel8@sha256:317149aaba6bbe1600330a381ba2f8a7c2aba36db4f7cbd68545e037cfeed9db" // registry.redhat.io/oadp/oadp-velero-plugin-for-aws-rhel8:1.2.5-3
+	VeleroGcpImageTag = "oadp-velero-plugin-for-gcp-rhel8@sha256:1556f9a9d3cf8920ecda5f2a568f7277d339ec2725d2fd4a844d590c483a3bd6" // registry.redhat.io/oadp/oadp-velero-plugin-for-gcp-rhel8:1.2.5-3
 )
